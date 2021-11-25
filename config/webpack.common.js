6a0a34a9e5091a18f02262c3a73a899f55cb0f1c
yusheng <1837534886@qq.com>
2021-11-25 17:20:29 +0800

    feat: change babel config and add tsconfig

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -54,7 +54,10 @@ module.exports = {
         test: /\.(jsx|tsx?)$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-react', '@babel/preset-typescript'],
+          presets: [
+            ['@babel/preset-react', { runtime: 'automatic' }],
+            '@babel/preset-typescript',
+          ],
         },
       },
     ],
